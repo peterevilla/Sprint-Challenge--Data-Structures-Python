5a940f5de806e4cd31ba2b37f89d65a23d4a381d
Pedro Revilla <peterevilla@gmail.com>
2020-07-31 22:20:14 -0500

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,23 +2,31 @@ class RingBuffer:
     def __init__(self, capacity):
         self.elements = []
         self.capacity = capacity
+        self.index = 0
 
     def append(self, item):
-        if len(self.elements) < self.capacity:
-            self.elements.append(item)
+        if len(self.elements) == self.capacity:
+            self.elements[self.index] = item
+            self.index = (self.index + 1) % self.capacity
+
         else:
-            self.elements[0] = item
+            self.elements.append(item)
+
+            if len(self.elements) == self.capacity:
+                self.index = 0
 
     def get(self):
         return self.elements
 
 
 
-buffer = RingBuffer(5)
+buffer = RingBuffer(3)
 buffer.append('a')
 buffer.append('b')
 buffer.append('c')
 buffer.append('d')
 buffer.append('e')
-buffer.append('e')
+buffer.append('f')
+buffer.append('g')
+buffer.append('h')
 print(buffer.get())
